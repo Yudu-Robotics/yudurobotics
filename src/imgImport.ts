bd5928ac4e51cd4647ec04ef976183515f71c08c
codewithsangshek <19001013003@jcboseust.ac.in>
2024-09-25 12:56:15 +0530

    fix: deployment image fix

--- src/imgImport.ts
+++ src/imgImport.ts
@@ -1,42 +1,42 @@
 //other
-const logo = "/assets/Logo.png";
-const favicon = "/assets/favicon.png";
+const logo = "assets/Logo.png";
+const favicon = "assets/favicon.png";
 
 // home
-const headerSection = "/assets/other/Section Seperators.svg";
-const footerSection = "/assets/home/FooterSection.png";
-const homeImage = "/assets/home/homeImage.png";
-const star1 = "/assets/home/star1.png";
-const star2 = "/assets/home/star2.png";
-const blurStar = "/assets/home/blurStar.png";
-const avtarGroup = "/assets/home/Avatar group.png";
-const appStore = "/assets/home/AppStoreBadge.png";
-const googlePlay = "/assets/home/GooglePlayBadge.png";
+const headerSection = "assets/other/Section Seperators.svg";
+const footerSection = "assets/home/FooterSection.png";
+const homeImage = "assets/home/homeImage.png";
+const star1 = "assets/home/star1.png";
+const star2 = "assets/home/star2.png";
+const blurStar = "assets/home/blurStar.png";
+const avtarGroup = "assets/home/Avatar group.png";
+const appStore = "assets/home/AppStoreBadge.png";
+const googlePlay = "assets/home/GooglePlayBadge.png";
 
 // zing
-const humanoid = "/assets/zing/Humanoid.png";
-const productBackground = "/assets/zing/productBackground.png";
-const productDetail1 = "/assets/zing/productDetail1.png";
+const humanoid = "assets/zing/Humanoid.png";
+const productBackground = "assets/zing/productBackground.png";
+const productDetail1 = "assets/zing/productDetail1.png";
 
 // other
-const arrow = "/assets/other/arrow.png";
-const searchIcon = "/assets/other/searchIcon.png";
-const arrowUpRight = "/assets/other/arrow-up-right.png";
-const handDrawArrow2 = "/assets/other/Hand-drawn-arrow-2.png";
-const iphone = "/assets/other/iphone.png";
-const monitorAndPhone = "/assets/other/monitor-phone.png";
-const imagePlaceholder = "/assets/other/imagePlaceholder.png";
-const imagePlaceholder2 = "/assets/other/imagePlaceholder2.png";
-const stars = "/assets/other/Stars.png";
-const girls = "/assets/other/girls.png";
-const macbook = "/assets/other/macbook.png";
-const videoPlayer = "/assets/other/video-player.png";
+const arrow = "assets/other/arrow.png";
+const searchIcon = "assets/other/searchIcon.png";
+const arrowUpRight = "assets/other/arrow-up-right.png";
+const handDrawArrow2 = "assets/other/Hand-drawn-arrow-2.png";
+const iphone = "assets/other/iphone.png";
+const monitorAndPhone = "assets/other/monitor-phone.png";
+const imagePlaceholder = "assets/other/imagePlaceholder.png";
+const imagePlaceholder2 = "assets/other/imagePlaceholder2.png";
+const stars = "assets/other/Stars.png";
+const girls = "assets/other/girls.png";
+const macbook = "assets/other/macbook.png";
+const videoPlayer = "assets/other/video-player.png";
 
 // peecee
-const peecee = "/assets/peecee/peecee.png";
-const avatar = "/assets/peecee/Avatar.png";
-const profileImage = "/assets/peecee/profileImage.png";
-const handArrow = "/assets/peecee/hand-arrow.png";
+const peecee = "assets/peecee/peecee.png";
+const avatar = "assets/peecee/Avatar.png";
+const profileImage = "assets/peecee/profileImage.png";
+const handArrow = "assets/peecee/hand-arrow.png";
 
 function renderImg(imgName: string) {
   switch (imgName) {
