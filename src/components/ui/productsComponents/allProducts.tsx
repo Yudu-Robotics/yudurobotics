674751bf7fb65bafbec5dd1e637df8d20c016424
avinashkumar1307 <evobi_dev@Evobis-Mac-mini.local>
2024-10-21 07:32:39 -0700

    Now image looking good

--- src/components/ui/productsComponents/allProducts.tsx
+++ src/components/ui/productsComponents/allProducts.tsx
@@ -1,61 +1,68 @@
-'use client'
-import React from 'react'
-import ProductCard from './product-card'
-import { products } from '@/data/mockData'
-import CustomHeading from '../common/custom-heading-props'
+"use client";
+import React from "react";
+import ProductCard from "./product-card";
+import { products } from "@/data/mockData";
+import CustomHeading from "../common/custom-heading-props";
+import Link from "next/link";
 
 interface Products {
-    name: string
-    description: string
-    image: string
-    link: string
-    type: string
-    category: string
+  name: string;
+  description: string;
+  image: string;
+  link: string;
+  type: string;
+  category: string;
 }
 
 interface AllProductsComponentProps {
-    products: Products[]
+  products: Products[];
 }
 
 const AllProductsComponent = () => {
-    const hardwareProducts = products.filter(
-        (product) => product.type === 'Hardware'
-    )
-    const softwareProducts = products.filter(
-        (product) => product.type === 'Software'
-    )
-    const CurriculumProducts = products.filter(
-        (product) => product.type === 'Curriculum'
-    )
+  const hardwareProducts = products.filter(
+    (product) => product.type === "Hardware"
+  );
+  const softwareProducts = products.filter(
+    (product) => product.type === "Software"
+  );
+  const CurriculumProducts = products.filter(
+    (product) => product.type === "Curriculum"
+  );
 
-    return (
-        <div className="container mx-auto p-4">
-            {/* Custom Heading */}
-            <CustomHeading title="Hardware" />
+  return (
+    <div className="container mx-auto p-4">
+      {/* Custom Heading */}
+      <CustomHeading title="Hardware" />
 
-            {/* Product Grid */}
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
-                {hardwareProducts.map((product, index) => (
-                    <ProductCard key={index} product={product} />
-                ))}
-            </div>
+      {/* Product Grid */}
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
+        {hardwareProducts.map((product, index) => (
+          <Link href={product.link} key={index}>
+            <ProductCard key={index} product={product} />
+          </Link>
+        ))}
+      </div>
 
-            <CustomHeading title="Software" />
+      <CustomHeading title="Software" />
 
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
-                {softwareProducts.map((product, index) => (
-                    <ProductCard key={index} product={product} />
-                ))}
-            </div>
-            <CustomHeading title="Curriculum" />
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
+        {softwareProducts.map((product, index) => (
+          <Link href={product.link} key={index}>
+            <ProductCard key={index} product={product} />
+          </Link>
+        ))}
+      </div>
+      <CustomHeading title="Curriculum" />
 
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
-                {CurriculumProducts.map((product, index) => (
-                    <ProductCard key={index} product={product} />
-                ))}
-            </div>
-        </div>
-    )
-}
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8">
+        {CurriculumProducts.map((product, index) => (
+          <Link href={product.link} key={index}>
+            <ProductCard key={index} product={product} />
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default AllProductsComponent
+export default AllProductsComponent;
--- src/components/ui/productsComponents/product-card.tsx
+++ src/components/ui/productsComponents/product-card.tsx
@@ -1,42 +1,42 @@
 interface ProductCardProps {
-    product: {
-        name: string
-        description: string
-        image: string
-        link: string
-    }
-    isSelected?: boolean
-    handleClick?: () => void
+  product: {
+    name: string;
+    description: string;
+    image: string;
+    link: string;
+  };
+  isSelected?: boolean;
+  handleClick?: () => void;
 }
 
 const ProductCard: React.FC<ProductCardProps> = ({
-    product,
-    isSelected = false, // Default value is false
-    handleClick = () => {}, // Default is an empty function
+  product,
+  isSelected = false, // Default value is false
+  handleClick = () => {}, // Default is an empty function
 }) => (
-    <div
-        onClick={handleClick}
-        className={`flex flex-col space-y-2 p-4 text-black rounded-lg transition-all duration-200 hover:bg-card-foreground hover:scale-90
-        ${isSelected ? 'bg-card-foreground scale-90' : ''}
+  <div
+    onClick={handleClick}
+    className={`flex flex-col space-y-2 p-4 text-black rounded-lg transition-all duration-200 hover:bg-card-foreground hover:scale-90
+        ${isSelected ? "bg-card-foreground scale-90" : ""}
       `}
-    >
-        {/* Product Image */}
-        <div className="w-full">
-            <img
-                src={product.image}
-                alt={product.name}
-                className="object-cover w-full"
-            />
-        </div>
-
-        {/* Product Title */}
-        <h3 className="text-lg font-bold mb-2 lg:text-xl">{product.name}</h3>
-
-        {/* Product Description */}
-        <p className="text-start text-secondary-foreground text-xs tracking-wide lg:text-sm">
-            {product.description}
-        </p>
+  >
+    {/* Product Image */}
+    <div className="w-full">
+      <img
+        src={product.image}
+        alt={product.name}
+        className="object-cover w-full"
+      />
     </div>
-)
 
-export default ProductCard
+    {/* Product Title */}
+    <h3 className="text-lg font-bold mb-2 lg:text-xl">{product.name}</h3>
+
+    {/* Product Description */}
+    <p className="text-start text-secondary-foreground text-xs tracking-wide lg:text-sm">
+      {product.description}
+    </p>
+  </div>
+);
+
+export default ProductCard;
