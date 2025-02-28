38c3b29c9023abd95aea287da56e4b8a980aa745
Jovit-Mathew236 <jovitmathew236@gmail.com>
2025-02-28 17:42:11 +0530

    refactor:removed all mp4 and changed to webm

--- src/videoImport.ts
+++ src/videoImport.ts
@@ -1,58 +1,58 @@
 // Crawle
-const spider = "assets/video/crawle/spider.mp4";
-const spider1 = "assets/video/crawle/spider1.mp4";
-const flexible = "assets/video/crawle/flexible.mp4";
-const friendly = "assets/video/crawle/friendly.mp4";
-const iteractive = "assets/video/crawle/iteractive.mp4";
-const tech = "assets/video/crawle/tech.mp4";
-const versatile = "assets/video/crawle/versatile.mp4";
-const stepcrawle = "assets/video/crawle/stepcrawle.mp4";
-const codingcrawle = "assets/video/crawle/codingcrawle.mp4";
-const aimodelcrawle = "assets/video/crawle/aimodelcrawle.mp4";
+const spider = "assets/video/crawle/spider.webm";
+const spider1 = "assets/video/crawle/spider1.webm";
+const flexible = "assets/video/crawle/flexible.webm";
+const friendly = "assets/video/crawle/friendly.webm";
+const iteractive = "assets/video/crawle/iteractive.webm";
+const tech = "assets/video/crawle/tech.webm";
+const versatile = "assets/video/crawle/versatile.webm";
+const stepcrawle = "assets/video/crawle/stepcrawle.webm";
+const codingcrawle = "assets/video/crawle/codingcrawle.webm";
+const aimodelcrawle = "assets/video/crawle/aimodelcrawle.webm";
 
 //Peecee
-const brainy = "assets/video/peecee/brainy.mp4";
-const buzz = "assets/video/peecee/buzz.mp4";
-const expressive = "assets/video/peecee/expressive.mp4";
-const power = "assets/video/peecee/power.mp4";
-const rainbow = "assets/video/peecee/rainbow.mp4";
-const sensors = "assets/video/peecee/sensors.mp4";
-const iot = "assets/video/peecee/iot.mp4";
-const plug = "assets/video/peecee/plug.mp4";
-const wire = "assets/video/peecee/wire.mp4";
-const live = "assets/video/peecee/plug.mp4";
-const ird = "assets/video/peecee/ird.mp4";
-const Joystickandswitches = "assets/video/peecee/Joystickandswitches.mp4";
-const LaserLedDisplay = "assets/video/peecee/LaserLedDisplay.mp4";
-const plugandplay = "assets/video/peecee/plugandplay.mp4";
-const steppeecee = "assets/video/peecee/3steppeecee.mp4";
-const codingpeecee = "assets/video/peecee/5codingpeecee.mp4";
-const aimodelpeecee = "assets/video/peecee/aimodelpeecee.mp4";
+const brainy = "assets/video/peecee/brainy.webm";
+const buzz = "assets/video/peecee/buzz.webm";
+const expressive = "assets/video/peecee/expressive.webm";
+const power = "assets/video/peecee/power.webm";
+const rainbow = "assets/video/peecee/rainbow.webm";
+const sensors = "assets/video/peecee/sensors.webm";
+const iot = "assets/video/peecee/iot.webm";
+const plug = "assets/video/peecee/plug.webm";
+const wire = "assets/video/peecee/wire.webm";
+// const live = "assets/video/peecee/plug.webm";
+const ird = "assets/video/peecee/ird.webm";
+const Joystickandswitches = "assets/video/peecee/Joystickandswitches.webm";
+const LaserLedDisplay = "assets/video/peecee/LaserLedDisplay.webm";
+const plugandplay = "assets/video/peecee/plugandplay.webm";
+const steppeecee = "assets/video/peecee/3steppeecee.webm";
+const codingpeecee = "assets/video/peecee/5codingpeecee.webm";
+const aimodelpeecee = "assets/video/peecee/aimodelpeecee.webm";
 
 // Roboki
-const iteractivero = "assets/video/roboki/interactive.mp4";
-const long = "assets/video/roboki/long.mp4";
-const playful = "assets/video/roboki/playful.mp4";
-const powerful = "assets/video/roboki/powerful.mp4";
-const wireless = "assets/video/roboki/wireless.mp4";
-const steprobo = "assets/video/roboki/steprobo.mp4";
-const codingrobo = "assets/video/roboki/codingrobo.mp4";
-const airobo = "assets/video/roboki/airobo.mp4";
+const iteractivero = "assets/video/roboki/interactive.webm";
+const long = "assets/video/roboki/long.webm";
+const playful = "assets/video/roboki/playful.webm";
+const powerful = "assets/video/roboki/powerful.webm";
+const wireless = "assets/video/roboki/wireless.webm";
+const steprobo = "assets/video/roboki/steprobo.webm";
+const codingrobo = "assets/video/roboki/codingrobo.webm";
+const airobo = "assets/video/roboki/airobo.webm";
 
 //Zing
-const stepzing = "assets/video/zing/stepzing.mp4";
-const codingzing = "assets/video/zing/5codingzing.mp4";
-const aimodelzing = "assets/video/zing/aimodelzing.mp4";
-const Interactivezing = "assets/video/zing/Interactivezing.mp4";
-const sensorzing = "assets/video/zing/sensorzing.mp4";
-const powerzing = "assets/video/zing/powerzing.mp4";
-const smartzing = "assets/video/zing/smartzing.mp4";
-const versatilezing = "assets/video/zing/versatilezing.mp4";
+const stepzing = "assets/video/zing/stepzing.webm";
+const codingzing = "assets/video/zing/5codingzing.webm";
+const aimodelzing = "assets/video/zing/aimodelzing.webm";
+const Interactivezing = "assets/video/zing/Interactivezing.webm";
+const sensorzing = "assets/video/zing/sensorzing.webm";
+const powerzing = "assets/video/zing/powerzing.webm";
+const smartzing = "assets/video/zing/smartzing.webm";
+const versatilezing = "assets/video/zing/versatilezing.webm";
 
 //klaw
-const stepklaw = "assets/video/klaw/stepklaw.mp4";
-const codingklaw = "assets/video/klaw/codingklaw.mp4";
-const aimodelklaw = "assets/video/klaw/aimodelklaw.mp4";
+const stepklaw = "assets/video/klaw/stepklaw.webm";
+const codingklaw = "assets/video/klaw/codingklaw.webm";
+const aimodelklaw = "assets/video/klaw/aimodelklaw.webm";
 
 function renderVideo(video: string) {
   switch (video) {
@@ -86,7 +86,7 @@ function renderVideo(video: string) {
     case "iot":
       return iot;
     case "plug":
-      plug;
+      return plug;
     case "wire":
       return wire;
     case "live":
