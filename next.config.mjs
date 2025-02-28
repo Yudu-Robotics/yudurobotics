f1298136937f193b250491fc9df26fa65b008451
sahil-bibox <sahil@biboxlabs.com>
2025-02-28 12:23:17 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
+  trailingSlash: true,
   eslint: {
     ignoreDuringBuilds: true,
   },
+  basePath: "/yudurobotics",
+  assetPrefix: "/yudurobotics/",
   async redirects() {
     return [
       {
