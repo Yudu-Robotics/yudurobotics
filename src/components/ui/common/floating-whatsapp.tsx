6d90d85c85de73421ef67d76d8865a7385d3555c
Jovit-Mathew236 <jovitmathew236@gmail.com>
2025-03-05 11:52:14 +0530

    feat:added new option for contact us in fixed whatsapp icon

--- src/components/ui/common/floating-whatsapp.tsx
+++ src/components/ui/common/floating-whatsapp.tsx
@@ -6,21 +6,25 @@ import {
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover";
+import { Dialog, DialogContent } from "@/components/ui/dialog";
+import ContactForm from "./contact-us-form";
 import { X } from "lucide-react";
 
 const FloatingWhatsApp = () => {
+  // const router = useRouter();
   const phoneNumber = "919606755032";
   const [isOpen, setIsOpen] = useState(false);
+  const [showContactForm, setShowContactForm] = useState(false);
 
   useEffect(() => {
-    const hasShownPopup = sessionStorage.getItem('whatsappPopupShown');
-    
+    const hasShownPopup = sessionStorage.getItem("whatsappPopupShown");
+
     if (!hasShownPopup) {
       const timer = setTimeout(() => {
         setIsOpen(true);
-        sessionStorage.setItem('whatsappPopupShown', 'true');
+        sessionStorage.setItem("whatsappPopupShown", "true");
       }, 5000);
-    
+
       return () => clearTimeout(timer);
     }
   }, []);
@@ -36,12 +40,12 @@ const FloatingWhatsApp = () => {
           type="button"
           onClick={handleClick}
           className="fixed bottom-20 sm:right-4 right-2 outline-none"
-          style={{ zIndex: 100 }}
+          style={{ zIndex: 50 }}
         >
           <div>{renderSvg("floatingWhatsApp")}</div>
         </button>
       </PopoverTrigger>
-      <PopoverContent className="w-80 p-0 bg-gray-50 mr-2 md:mr-4">
+      <PopoverContent className="w-80 p-0 bg-gray-50 mr-2 md:mr-4 rounded-lg">
         <div className="flex justify-between items-center bg-green-600 p-3 text-white rounded-t-lg">
           <h4 className="font-medium">Chat with us!</h4>
           <button
@@ -57,15 +61,43 @@ const FloatingWhatsApp = () => {
             <p className="text-sm text-gray-600 mb-1">Hi! 👋</p>
             <p className="text-sm text-gray-600">How can i help you</p>
           </div>
-          <button
-            type="button"
-            onClick={handleClick}
-            className="w-full bg-green-600 text-white py-2 px-4 rounded-lg hover:bg-green-700 transition-colors"
-          >
-            Start Chatting
-          </button>
+          <div className="flex gap-2">
+            <button
+              type="button"
+              onClick={handleClick}
+              className="w-full bg-green-600 text-white py-2 px-4 rounded-md hover:bg-green-700 transition-colors"
+            >
+              WhatsApp
+            </button>
+            <button
+              type="button"
+              onClick={() => {
+                setShowContactForm(true);
+              }}
+              className="w-full bg-primary text-white py-2 px-4 rounded-md hover:bg-secondary transition-colors"
+            >
+              Contact Us
+            </button>
+          </div>
         </div>
       </PopoverContent>
+
+      {/* Add Dialog for Contact Form */}
+      <Dialog open={showContactForm} onOpenChange={setShowContactForm}>
+        <DialogContent className="sm:max-w-[600px] p-1 bg-transparent border-none">
+          <div className="flex justify-end  absolute top-2 right-2">
+            <button
+              type="button"
+              name="close"
+              onClick={() => setShowContactForm(false)}
+              className="p-2 hover:bg-gray-100 rounded-full"
+            >
+              <X className="h-4 w-4" color="black" />
+            </button>
+          </div>
+          <ContactForm />
+        </DialogContent>
+      </Dialog>
     </Popover>
   );
 };
