3ae93a6404c61f56d73ed42ebabb5cb64e011f19
Jovit-Mathew236 <jovitmathew236@gmail.com>
2025-03-05 13:35:46 +0530

    build error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
-// import { redirect } from "next/navigation";
+import { redirect } from "next/navigation";
 
 export default async function Page() {
-  return {
-    redirect: {
-      destination: "/home",
-      permanent: true, // triggers 308
-    },
-  };
+  // Trigger the redirect
+  redirect("/home");
+  return null; // Ensure the function returns null after the redirect
 }
+
+// export async function Page() {
+//   return {
+//     redirect: {
+//       destination: "/home",
+//       permanent: true, // triggers 308
+//     },
+//   };
+// }
