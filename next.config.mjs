1b5f3b2f9aa0942089a6afab23a975cce44d8752
sahild91 <sahildiwakar91@gmail.com>
2025-03-07 14:29:27 +0530

    Update config for GitHub Actions

--- next.config.mjs
+++ next.config.mjs
@@ -21,6 +21,7 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   images: {
+    unoptimized: true,
     formats: ["image/avif", "image/webp"],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     minimumCacheTTL: 60,
