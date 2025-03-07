150d14ec4e249839bd7652b679c361807fd3927f
Jovit-Mathew236 <jovitmathew236@gmail.com>
2025-03-07 15:59:12 +0530

    updated meta data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -44,13 +44,23 @@ const ttHovesBold = localFont({
 // import { Inter } from "next/font/google";
 import { ThemeProvider } from "@/components/theme-provider";
 import "./globals.css";
-import Head from "next/head";
 
 // const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Yudo Robotics",
-  description: "LMS",
+  title: "Yudu Robotics",
+  description:
+    "From Playful Robots and Drones to Precision 3D Printers Unleash the power of creative robotics. Yudu makes the impossible fun, helping you build, play, and explore with groundbreaking tech.",
+  openGraph: {
+    title: "Yudu Robotics",
+    description:
+      "From Playful Robots and Drones to Precision 3D Printers Unleash the power of creative robotics. Yudu makes the impossible fun",
+    images: [
+      {
+        url: "/assets/favicon.ico",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
@@ -63,20 +73,6 @@ export default function RootLayout({
       lang="en"
       className={`${cobaltRidge.variable} ${ttHovesRegular.variable} ${ttHovesMedium.variable} ${ttHovesDemiBold.variable} ${ttHovesBold.variable} w-screen overflow-x-hidden`}
     >
-      <Head>
-        <title>Yudu Robotics</title>
-        <meta
-          name="description"
-          content="From Playful Robots and Drones to Precision 3D Printers Unleash the power of creative robotics. Yudu makes the impossible fun, helping you build, play, and explore with groundbreaking tech."
-          key="desc"
-        />
-        <meta property="og:title" content="Yudu Robotics" />
-        <meta
-          property="og:description"
-          content="From Playful Robots and Drones to Precision 3D Printers Unleash the power of creative robotics. Yudu makes the impossible fun, "
-        />
-        <meta property="og:image" content="/assets/favicon.ico" />
-      </Head>
       <body>
         <ThemeProvider
           attribute="class"
